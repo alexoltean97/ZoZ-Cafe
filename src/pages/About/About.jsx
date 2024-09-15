13c5ec6d457707664d94682af139edde724858dc
alexoltean97 <alex.oltean97@gmail.com>
2024-09-15 15:36:30 +0300

    More work

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,38 @@
 const About = () => {
   return (
-    <div>
-      <span>Despre noi</span>
-      <h4></h4>
+    <div className="main-about">
+      <h2>Despre noi</h2>
+      <div className="row">
+        <div className="col-lg-9">
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu
+            risus non turpis vestibulum ullamcorper. Suspendisse ut quam
+            eleifend, feugiat nunc sit amet, dictum magna. Nulla malesuada non
+            purus vel elementum. Quisque efficitur ultrices arcu vitae viverra.
+            In arcu risus, tincidunt sodales posuere a, venenatis sed ex. Nullam
+            cursus aliquet ante id porta. Mauris maximus diam vel mattis
+            consequat. Integer consectetur leo ut magna accumsan, eu cursus
+            purus consequat. Mauris aliquam mattis lobortis. Phasellus sit amet
+            quam quis lectus bibendum aliquam. Sed tempus erat pulvinar diam
+            porta pulvinar. Phasellus porta suscipit hendrerit. Curabitur nisl
+            orci, iaculis in elit et, euismod sagittis diam. Curabitur enim
+            ipsum, laoreet quis est at, egestas accumsan quam. Sed vitae lorem
+            eget nunc congue efficitur. Quisque ultrices leo leo, et hendrerit
+            leo blandit at. Cras vitae nisl at tortor vestibulum aliquet eu eu
+            ante. Nulla ante sapien, gravida sed condimentum at, luctus id
+            dolor. Nullam consequat augue sapien, a finibus metus lobortis non.
+            Aliquam posuere varius erat, eget ornare nunc ultrices eu. Proin
+            feugiat sit amet neque vel ultrices. Morbi nunc massa, feugiat eget
+            laoreet sit amet, tristique in ante.
+          </div>
+        </div>
+
+        <div class="col-lg-3">
+          <div className="images-content">
+            <img src="https://placehold.co/400"></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
