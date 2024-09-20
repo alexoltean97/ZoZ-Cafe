937283a5300924004a02c7002b432b316eafbbdf
alexoltean97 <alex.oltean97@gmail.com>
2024-09-20 08:48:00 +0300

    Categories Filter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,19 @@
-import Banner from '../../components/Banner/Banner';
-import Categories from '../../components/Categories/Categories';
-import Products from '../../components/Products/Products';
+import { useState } from "preact/hooks";
+import Banner from "../../components/Banner/Banner";
+import Categories from "../../components/Categories/Categories";
+import Products from "../../components/Products/Products";
+
 const Home = () => {
+  const [activeCategory, setActiveCategory] = useState("espresso");
+
   return (
     <>
-
-    <Banner />
-    <Categories />
-    <Products />
+      <Banner />
+      <Categories
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
+      <Products activeCategory={activeCategory} />
     </>
   );
 };
