cabab0d5fe4fb26c7a88af9832e92072600cbf98
alexoltean97 <alex.oltean97@gmail.com>
2024-10-03 19:33:03 +0300

    Some work

--- src/components/ProductTabContent/ProductTabContent.jsx
+++ src/components/ProductTabContent/ProductTabContent.jsx
@@ -3,46 +3,53 @@ const ProductTabContent = ({ activeTab, product }) => {
     <div className="tab-content mt-4">
       {activeTab === "description" && (
         <div className="tab-pane active" id="description">
-          <h3>Description</h3>
           <p>{product.description}</p>
         </div>
       )}
       {activeTab === "ingredients" && (
         <div className="tab-pane active" id="ingredients">
-          <h3>Ingredients</h3>
           <p>{product.ingredients}</p>
         </div>
       )}
       {activeTab === "extras" && (
         <div className="tab-pane active" id="extras">
-          <h3>Extras</h3>
-          {product.extrashot && (
-            <div className="extra-item">
-              <strong>{product.extrashot.name}</strong> -{" "}
-              {product.extrashot.description} ({product.extrashot.weight})
-            </div>
-          )}
-          {product.extraMilkOat && (
-            <div className="extra-item">
-              <strong>{product.extraMilkOat.name}</strong> -{" "}
-              {product.extraMilkOat.description} ({product.extraMilkOat.weight})
-            </div>
-          )}
-          {product.extraMilkPeas && (
-            <div className="extra-item">
-              <strong>{product.extraMilkPeas.name}</strong> -{" "}
-              {product.extraMilkPeas.description} (
-              {product.extraMilkPeas.weight})
-            </div>
+          {!product.extrashot &&
+          !product.extraMilkOat &&
+          !product.extraMilkPeas ? (
+            <div>No extras are available for this product</div>
+          ) : (
+            <>
+              {product.extrashot && (
+                <div className="extra-item">
+                  <strong>{product.extrashot.name}</strong> -{" "}
+                  {product.extrashot.description} ({product.extrashot.weight})
+                </div>
+              )}
+              {product.extraMilkOat && (
+                <div className="extra-item">
+                  <strong>{product.extraMilkOat.name}</strong> -{" "}
+                  {product.extraMilkOat.description} (
+                  {product.extraMilkOat.weight})
+                </div>
+              )}
+              {product.extraMilkPeas && (
+                <div className="extra-item">
+                  <strong>{product.extraMilkPeas.name}</strong> -{" "}
+                  {product.extraMilkPeas.description} (
+                  {product.extraMilkPeas.weight})
+                </div>
+              )}
+            </>
           )}
         </div>
       )}
       {activeTab === "alergens" && (
         <div className="tab-pane active" id="alergens">
-          <h3>Alergens</h3>
-          <p>
-            {product.alergens ? product.alergens : "No alergens specified."}
-          </p>
+          {product.alergens && product.alergens.length > 0 ? (
+            <p>{product.alergens}</p>
+          ) : (
+            <p>No allergens specified.</p>
+          )}
         </div>
       )}
     </div>
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -21,20 +21,21 @@ const Product = () => {
   return (
     <div className="container my-4">
       <div className="product-title">
-        <h3>{product.title}</h3>
+        <h2 className="mb-3">{product.title}</h2>
+        <h5 className="mb-3">Produs disponibil doar in cafenea</h5>
       </div>
 
       <div className="row">
-        <div className="col-lg-3">
-        <ProductImage />
+        <div className="col-lg-3 col-sm-12 col-12 order-2 order-lg-1">
+          <ProductImage />
         </div>
-    
-        <div className="col-lg-9">
-        <div className="product-info">
-          <ProductTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-          <ProductTabContent activeTab={activeTab} product={product} />
+
+        <div className="col-lg-9 col-sm-12 col-12 order-1 order-lg-2">
+          <div className="product-info">
+            <ProductTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+            <ProductTabContent activeTab={activeTab} product={product} />
+          </div>
         </div>
-    </div>
       </div>
     </div>
   );
