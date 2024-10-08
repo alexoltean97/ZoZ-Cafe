3b6e4d81137b618749202c14a4d9b0ad42b3402a
alexoltean97 <alex.oltean97@gmail.com>
2024-10-08 19:43:57 +0300

    Jesus

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -41,7 +41,7 @@ const Product = () => {
         <div className="row">
           <div className="col-lg-3 col-sm-12 col-12 order-2 order-lg-1">
             <ProductImage
-               src={`/images/${item.image}`} 
+               src={`/images/${product.image}`} 
               alt={product.title}
             />
           </div>
